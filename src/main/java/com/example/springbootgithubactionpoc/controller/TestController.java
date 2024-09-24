acbafec89473ba00fdfe4a9f71f40aa340d139c4
Vincent <va543567@gmail.com>
2024-09-24 11:18:21 +0100

    init project

---
+++ src/main/java/com/example/springbootgithubactionpoc/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.springbootgithubactionpoc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/greeting")
+    @ResponseBody
+    public String greeting() {
+        return "Hello World!";
+    }
+
+}
---
+++ src/test/java/com/example/springbootgithubactionpoc/SpringBootGithubActionPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootgithubactionpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGithubActionPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
